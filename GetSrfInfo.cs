cc41baaf3138c31f53cedf698c8dc96583c83eb9
igorvar <igorvar@users.noreply.github.com>
2025-08-26 13:33:07 +0300

    Add files via upload

---
+++ GetSrfInfo.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenMcdf;
+
+namespace SrfInfoInner
+{
+    public static class GetSrfInfo
+    {
+        /// <summary>
+        /// return array of two SrfInfo - [0] info about full compile, [1] - about current compile. It is possible that Current == full, it means [1].IsFullCompile = true, otherwise = false;
+        /// </summary>
+        /// <param name="SrfFile"></param>
+        /// <returns></returns>
+        public static SrfMetaData[] GetInfo(string SrfFile)
+        {
+            SrfMetaData fullCompile;
+            SrfMetaData currentCompile;
+            using (OpenMcdf.CompoundFile cf = new CompoundFile(SrfFile, CFSUpdateMode.ReadOnly, CFSConfiguration.Default))
+            {
+                CFStream stream = null;
+                stream = cf.RootStorage.TryGetStream("Full Compile");
+                fullCompile = ReadBuffer(stream.GetData());
+                fullCompile.Srf = new FileInfo(SrfFile);
+                fullCompile.IsFullCompile = true;
+
+                stream = cf.RootStorage.TryGetStream("Last Incr. Compile");
+                if (stream == null)
+                    currentCompile = fullCompile;
+                else
+                {
+                    currentCompile = ReadBuffer(stream.GetData());
+                    currentCompile.Srf = new FileInfo(SrfFile);
+                    currentCompile.IsFullCompile = false;
+                }
+            }
+            return new SrfMetaData[2] { fullCompile, currentCompile };
+        }
+        private static SrfMetaData ReadBuffer(byte[] buffer)
+        {
+            /*string strUDT = Convert.ToString(buffer[7], 16).PadLeft(2, '0') + 
+                                  Convert.ToString(buffer[6], 16).PadLeft(2, '0') + 
+                                  Convert.ToString(buffer[5], 16).PadLeft(2, '0') + 
+                                  Convert.ToString(buffer[4], 16).PadLeft(2, '0');
+                unixDt = Convert.ToInt64(strUDT, 16);*/
+
+            //Same as:
+            /*unixDt = buffer[4] + 
+                     buffer[5] * 16 * 16 + 
+                     buffer[6] * 16 * 16 * 16 * 16 + 
+                     buffer[7] * 16 * 16 * 16 * 16 * 16 * 16;*/
+            //same as:
+            double unixDt = buffer[4] + (buffer[5] << 8) + (buffer[6] << 16) + (buffer[7] << 24);
+            DateTime winDt = UnixTimeStampToDateTime(unixDt);
+            
+            string mashineName = "";
+            for (int i = 8; i < 0x87 && buffer[i] != 0; i++) mashineName += (char)buffer[i];
+
+            string language = "";
+            for (int i = 0x88; i < 0x91 && buffer[i] != 0; i++) language += (char)buffer[i];
+
+            string login = "";
+            int bsFolderSuffix = 0;
+            for (int i = 0x8c; i < 0xAB && buffer[i] != 0; i++)
+            {
+                login += (char)buffer[i];
+                bsFolderSuffix += buffer[i];
+            }
+            SrfMetaData si = new SrfMetaData("",false,winDt,login,mashineName,language,$"srf{unixDt}_{bsFolderSuffix}");
+            return si;
+        }
+
+        private static DateTime UnixTimeStampToDateTime(double unixTimeStamp)
+        {
+            // Unix timestamp is seconds past epoch
+            DateTime epoch = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            return epoch.AddSeconds(unixTimeStamp).ToLocalTime();
+        }
+    }
+
+    public struct SrfMetaData
+    {
+        
+        public SrfMetaData(string Srf, bool IsFullCompile, DateTime CompilationDate, string CompiledBy, string MashineName, string Language, string BsFolder)
+        {
+            this.IsFullCompile = IsFullCompile;
+            this.CompilationDate = CompilationDate;
+            this.CompiledBy = CompiledBy;
+            this.MashineName = MashineName;
+            this.Language = Language;
+            this.BsFolder = BsFolder;
+            this.Srf = String.IsNullOrEmpty(Srf) ?  null : new FileInfo(Srf);
+        }
+        public bool IsFullCompile { get; internal set; }
+        public DateTime CompilationDate { get; private set; }
+        public string CompiledBy { get; private set; }
+        public string MashineName { get; private set; }
+        public string Language { get; private set; }
+        public string BsFolder { get; private set; }
+        public FileInfo Srf { get; internal set; }
+
+        public override string ToString()
+        {
+            return
+                $"IsFullCompile\t{IsFullCompile}\n" +
+                $"CompilationDate\t{CompilationDate}\n" +
+                $"CompiledBy\t{CompiledBy}\n" +
+                $"MashineName\t{MashineName}\n" +
+                $"Language\t{Language}\n" +
+                $"BsFolder\t{BsFolder}";
+        }
+
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Diagnostics;
+using System.IO;
+using SrfInfoInner;
+using System.Xml.Linq;
+using RedBlackTree;
+using System.Security.Cryptography;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using OpenMcdf;
+namespace SrfInfo
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            SrfMetaData[] res;// = null;
+            string fileName = String.Empty;//= @"C:\oracle\siebel\Client\objects\enu\siebel_sia.srf";
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Return metadata of Siebel srf file.");
+                Console.WriteLine("Parameters:");
+                Console.WriteLine("\t srf file");
+                Console.WriteLine("\t /fn: n number 0...15 - color of text. (https://learn.microsoft.com/dotnet/api/system.consolecolor)");
+                Console.WriteLine("\t /bn: n number 0...15 - color of background");
+                Console.WriteLine("Example: C:\\siebel\\client\\objects\\enu\\siebel_sia.srf /f2 /b15");
+                Console.Write("Srf: ");
+                fileName = Console.ReadLine();
+            }
+            else
+            {
+                int clr;
+                ConsoleColor fColor = Console.ForegroundColor;
+                ConsoleColor bColor = Console.BackgroundColor;
+                for (int i = 0; i < args.Length; i++)
+                {
+                    if (args[i].StartsWith("/f") || args[i].StartsWith("/F"))
+
+                        if (int.TryParse(args[i].Substring(2), out clr))
+                            try { fColor = (ConsoleColor)Enum.GetValues(typeof(ConsoleColor)).GetValue(clr); }
+                            catch (System.IndexOutOfRangeException) { Console.WriteLine($"Value of {args[i].Substring(0, 2)} expected 0...15"); }
+                        else
+                            Console.WriteLine($"Invalid parameter {args[i]}. Expected /f and number 0...16 (/f9), instead of {args[i]}");
+                    else if (args[i].StartsWith("/b") || args[i].StartsWith("/B"))
+                        if (int.TryParse(args[i].Substring(2), out clr))
+                            try { bColor = (ConsoleColor)Enum.GetValues(typeof(ConsoleColor)).GetValue(clr); }
+                            catch (System.IndexOutOfRangeException) { Console.WriteLine($"Value of {args[i].Substring(0, 2)} expected 0...15"); }
+                        //Console.BackgroundColor = (ConsoleColor)Enum.GetValues(typeof(ConsoleColor)).GetValue(clr);
+                        //                    Console.Clear();
+                        else
+                            Console.WriteLine($"Invalid parameter {args[i]}. Expected /b and number 0...15 (/b10), instead of {args[i]}");
+                    else
+                        fileName = args[i];
+                }
+                if (fColor == bColor)
+                    Console.WriteLine($"The font and background colors are the same. Not changed ({(int)bColor})");
+                else
+                {
+                    Console.ForegroundColor = fColor;
+                    if (Console.BackgroundColor != bColor)
+                    {
+                        Console.BackgroundColor = bColor;
+                        Console.Clear();
+                    }
+                }
+                Console.WriteLine(fileName);
+            }
+            //}
+            if (!File.Exists(fileName))
+                throw new FileNotFoundException($"Not found file '{fileName}'");
+
+            res = GetSrfInfo.GetInfo(fileName);
+            Console.WriteLine(res[1].ToString());
+            if (!res[1].IsFullCompile)
+            {
+                Console.WriteLine("\n----------- First compile: -----------");
+                Console.WriteLine(res[0]);
+            }
+            res[0].IsFullCompile = true;
+            Console.ReadLine();
+        }
+        //static void Main1(string[] args)
+        //{
+        //    bool isFullCompile = false;
+        //    byte[] buffer = null;
+        //    string login = "";
+        //    int bsFolderSuffix = 0;
+        //    string bsFolderName;
+        //    string mashineName = "";
+        //    string language = "";
+        //    double unixDt;
+        //    DateTime winDt;
+        //    string fileName;//= @"C:\Users\Igor_var\Desktop\1973.srf";
+        //    try
+        //    {
+
+        //        if (args.Length == 0)
+        //        {
+        //            Console.Write("Srf: ");
+        //            fileName = Console.ReadLine();
+        //        }
+        //        else
+        //        {
+        //            fileName = args[0];
+        //            Console.WriteLine(fileName);
+        //        }
+        //        if (!File.Exists(fileName))
+        //            throw new FileNotFoundException($"Not found file '{fileName}'");
+
+
+        //        using (OpenMcdf.CompoundFile cf = new CompoundFile(fileName, CFSUpdateMode.ReadOnly, CFSConfiguration.Default))
+        //        {
+        //            CFStream stream = null;
+        //            stream = cf.RootStorage.TryGetStream("Last Incr. Compile");
+        //            if (stream == null)
+        //            {
+        //                isFullCompile = true;
+        //                stream = cf.RootStorage.TryGetStream("Full Compile");
+        //            }
+        //            buffer = stream.GetData();
+        //        }
+
+        //        /*string strUDT = Convert.ToString(buffer[7], 16).PadLeft(2, '0') + 
+        //                          Convert.ToString(buffer[6], 16).PadLeft(2, '0') + 
+        //                          Convert.ToString(buffer[5], 16).PadLeft(2, '0') + 
+        //                          Convert.ToString(buffer[4], 16).PadLeft(2, '0');
+        //        unixDt = Convert.ToInt64(strUDT, 16);*/
+
+        //        //Same as:
+        //        /*unixDt = buffer[4] + 
+        //                 buffer[5] * 16 * 16 + 
+        //                 buffer[6] * 16 * 16 * 16 * 16 + 
+        //                 buffer[7] * 16 * 16 * 16 * 16 * 16 * 16;*/
+        //        //same as:
+        //        unixDt = buffer[4] + (buffer[5] << 8) + (buffer[6] << 16) + (buffer[7] << 24);
+        //        winDt = UnixTimeStampToDateTime(unixDt);
+
+        //        for (int i = 8; i < 0x87 && buffer[i] != 0; i++) mashineName += (char)buffer[i];
+
+        //        for (int i = 0x88; i < 0x91 && buffer[i] != 0; i++) language += (char)buffer[i];
+
+        //        for (int i = 0x8c; i < 0xAB && buffer[i] != 0; i++)
+        //        {
+        //            login += (char)buffer[i];
+        //            bsFolderSuffix += buffer[i];
+        //        }
+        //        bsFolderName = $"srf{unixDt}_{bsFolderSuffix}";
+        //        Console.WriteLine($"IsFullCompile\t{isFullCompile}");
+        //        Console.WriteLine($"CompilationDate\t{winDt}");
+        //        Console.WriteLine($"CompiledBy\t{login}");
+        //        Console.WriteLine($"MashineName\t{mashineName}");
+        //        Console.WriteLine($"Language\t{language}");
+        //        Console.WriteLine($"BsFolder\t{bsFolderName}");
+        //    }
+        //    catch (Exception ex)
+        //    {
+
+        //        Console.WriteLine(ex.Message);
+        //    }
+
+        //    Console.ReadLine();
+        //}
+
+        //public static DateTime UnixTimeStampToDateTime(double unixTimeStamp)
+        //{
+        //    // Unix timestamp is seconds past epoch
+        //    DateTime epoch = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+        //    return epoch.AddSeconds(unixTimeStamp).ToLocalTime();
+        //}
+
+    }
+}
